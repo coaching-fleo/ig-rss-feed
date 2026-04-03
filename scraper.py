55a385a0de37fde0c0ae2bc0e410d29b531808ee
coaching-fleo <coaching@federicoleo.it>
2026-04-03 13:28:04 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,22 +1,37 @@
 import instaloader
 from feedgen.feed import FeedGenerator
 import datetime
+import os
 
 # 1. Impostazioni
 L = instaloader.Instaloader(quiet=True)
 PROFILE = "federico_leo88"
 
-# 2. Inizializza il Feed RSS
+# 2. Recupera le credenziali segrete da GitHub
+IG_USER = os.environ.get("IG_USER")
+IG_PASS = os.environ.get("IG_PASS")
+
+# 3. Inizializza il Feed RSS
 fg = FeedGenerator()
 fg.title(f'Instagram Feed - {PROFILE}')
 fg.link(href=f'https://instagram.com/{PROFILE}', rel='alternate')
 fg.description(f'Ultimi post di {PROFILE} su Instagram')
 
 try:
-    # 3. Scarica i dati del profilo (senza login)
+    # 4. Fai il Login con l'account finto per bypassare i blocchi
+    if IG_USER and IG_PASS:
+        try:
+            L.login(IG_USER, IG_PASS)
+            print("Login effettuato con successo.")
+        except Exception as e:
+            print(f"Errore di login: {e}")
+    else:
+        print("ATTENZIONE: Credenziali non trovate nei Secrets.")
+
+    # 5. Scarica i dati del profilo
     profile = instaloader.Profile.from_username(L.context, PROFILE)
     
-    # 4. Estrae gli ultimi 10 post
+    # 6. Estrae gli ultimi 10 post
     for i, post in enumerate(profile.get_posts()):
         if i >= 10:
             break
@@ -26,15 +41,12 @@ try:
         fe.title(f'Post del {post.date_utc.strftime("%d/%m/%Y")}')
         fe.link(href=f'https://instagram.com/p/{post.shortcode}')
         
-        # Crea il contenuto: immagine + testo
         caption = post.caption if post.caption else ''
         fe.description(f'<img src="{post.url}" /><br><p>{caption}</p>')
-        
-        # Aggiunge la data formattata correttamente per i feed RSS
         fe.pubDate(post.date_utc.replace(tzinfo=datetime.timezone.utc))
         
 except Exception as e:
     print(f"Errore durante l'estrazione: {e}")
 
-# 5. Salva il file
+# 7. Salva il file
 fg.rss_file('feed.xml')
